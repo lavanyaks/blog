ee6da06bf5372ce2dee23c8b217fcc47833a6152
lavanyaks <lavanya.singh24@gmail.com>
2017-08-17 11:30:58 -0400

    Adding more views

--- blog/views.py
+++ blog/views.py
@@ -7,4 +7,8 @@ from django.shortcuts import render_to_response, get_object_or_404
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
